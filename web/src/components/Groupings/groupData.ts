94b7a78e63bd9a0cf13c2344f1281fae14dfdfda
Tobias Beese <tobias.beese@gmx.de>
2021-12-10 16:15:50 +0100

    🐛 Sort Groupings by records length

--- web/src/components/Groupings/groupData.ts
+++ web/src/components/Groupings/groupData.ts
@@ -4,10 +4,14 @@ import type { Record } from 'types/Record'
 
 export const groupByArtikel = (list: Record[]) => {
   const groups = group(list, (d) => d.bezeichnung)
-  return Array.from(groups).sort((a, b) => descending(a.length, b.length))
+  return Array.from(groups).sort(([_a, a], [_b, b]) =>
+    descending(a.length, b.length)
+  )
 }
 
 export const groupByFehler = (list: Record[]) => {
   const groups = group(list, (d) => d.fehlerText)
-  return Array.from(groups).sort((a, b) => descending(a.length, b.length))
+  return Array.from(groups).sort(([_a, a], [_b, b]) =>
+    descending(a.length, b.length)
+  )
 }
