dc706c4538312825da213f0bf473e8e53943c6f5
Tobias Beese <tobias.beese@gmx.de>
2022-01-12 13:24:18 +0100

    Show resulting Grid in Geometrie Component

--- web/src/components/Geometrie/Geometrie/Geometrie.tsx
+++ web/src/components/Geometrie/Geometrie/Geometrie.tsx
@@ -2,6 +2,10 @@ import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 import { Link, routes, navigate } from '@redwoodjs/router'
 
+import type { FindGeometrieById } from 'types/graphql'
+
+import Grid from 'src/components/Grid'
+
 const DELETE_GEOMETRIE_MUTATION = gql`
   mutation DeleteGeometrieMutation($id: Int!) {
     deleteGeometrie(id: $id) {
@@ -10,7 +14,9 @@ const DELETE_GEOMETRIE_MUTATION = gql`
   }
 `
 
-const Geometrie = ({ geometrie }) => {
+const Geometrie = ({
+  geometrie: { id, Bezeichnung, rows, columns, pixels, image },
+}: FindGeometrieById) => {
   const [deleteGeometrie] = useMutation(DELETE_GEOMETRIE_MUTATION, {
     onCompleted: () => {
       toast.success('Geometrie gelöscht')
@@ -29,35 +35,34 @@ const Geometrie = ({ geometrie }) => {
       <div className="rw-segment">
         <header className="rw-segment-header">
           <h2 className="rw-heading rw-heading-secondary">
-            Geometrie &quot;{geometrie.Bezeichnung}&quot;
+            Geometrie &quot;{Bezeichnung}&quot;
           </h2>
         </header>
         <table className="rw-table">
           <tbody>
             <tr>
               <th>Id</th>
-              <td>{geometrie.id}</td>
+              <td>{id}</td>
             </tr>
             <tr>
               <th>Bezeichnung</th>
-              <td>{geometrie.Bezeichnung}</td>
-            </tr>
-            <tr>
-              <th>Zeilen</th>
-              <td>{geometrie.rows}</td>
-            </tr>
-            <tr>
-              <th>Spalten</th>
-              <td>{geometrie.columns}</td>
-            </tr>
-            <tr>
-              <th>Pixel</th>
-              <td>{geometrie.pixels}</td>
+              <td>{Bezeichnung}</td>
             </tr>
             <tr>
               <th>Grafik</th>
               <td>
-                <img src={geometrie.image} alt={geometrie.Bezeichnung} />
+                <img src={image} alt={Bezeichnung} />
+              </td>
+            </tr>
+            <tr>
+              <th>Raster</th>
+              <td>
+                <Grid
+                  data={[]}
+                  grid={{ rows, columns, pixels }}
+                  image={image}
+                  onClick={() => {}}
+                />
               </td>
             </tr>
           </tbody>
@@ -65,7 +70,7 @@ const Geometrie = ({ geometrie }) => {
       </div>
       <nav className="rw-button-group">
         <Link
-          to={routes.editGeometrie({ id: geometrie.id })}
+          to={routes.editGeometrie({ id })}
           className="rw-button rw-button-blue"
         >
           Bearbeiten
@@ -73,7 +78,7 @@ const Geometrie = ({ geometrie }) => {
         <button
           type="button"
           className="rw-button rw-button-red"
-          onClick={() => onDeleteClick(geometrie.id)}
+          onClick={() => onDeleteClick(id)}
         >
           Löschen
         </button>
