88e38319902a2278a8ec0dc36bb0160d514f8aa5
Tobias Beese <tobias.beese@gmx.de>
2022-06-20 11:27:03 +0200

    Fix broken api tests

--- api/src/services/buchungs/buchungs.scenarios.ts
+++ api/src/services/buchungs/buchungs.scenarios.ts
@@ -3,10 +3,18 @@ import type { Prisma } from '@prisma/client'
 export const standard = defineScenario<Prisma.BuchungCreateArgs>({
   buchung: {
     one: {
-      data: { datum: '2021-10-02T09:00:41Z', uhrzeit: '2021-10-02T09:00:41Z' },
+      data: {
+        datum: '2021-10-02T09:00:41Z',
+        uhrzeit: '2021-10-02T09:00:41Z',
+        auslaufzeit: '2021-10-02T08:00:41Z',
+      },
     },
     two: {
-      data: { datum: '2021-10-02T09:00:41Z', uhrzeit: '2021-10-02T09:00:41Z' },
+      data: {
+        datum: '2021-10-02T09:00:41Z',
+        uhrzeit: '2021-10-02T09:00:41Z',
+        auslaufzeit: '2021-10-02T08:00:41Z',
+      },
     },
   },
 })
