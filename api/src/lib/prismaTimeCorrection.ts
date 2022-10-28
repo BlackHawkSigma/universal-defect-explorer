472808a3b774891e4cd609fc745532b10f48d5ec
Tobias Beese <tobias.beese@gmx.de>
2022-10-28 14:59:23 +0200

    Add timezone correction middleware to prisma

---
+++ api/src/lib/prismaTimeCorrection.ts
@@ -0,0 +1,63 @@
+import type { Prisma } from '@prisma/client'
+import addMinutes from 'date-fns/addMinutes'
+import parseISO from 'date-fns/parseISO'
+import subMinutes from 'date-fns/subMinutes'
+
+const toLocalTime = (date: Date | string): Date => {
+  const time = date instanceof Date ? date : parseISO(date)
+  return addMinutes(time, time.getTimezoneOffset())
+}
+
+const toUTCTime = (date: Date | string): Date => {
+  const time = date instanceof Date ? date : parseISO(date)
+  return subMinutes(time, time.getTimezoneOffset())
+}
+
+// https://github.com/prisma/prisma/issues/5051#issuecomment-878106427
+
+const correctToLocal = (object?: Record<string, unknown>) => {
+  if (!object) return
+
+  for (const key of Object.keys(object)) {
+    const value = object[key]
+
+    if (value instanceof Date) {
+      object[key] = toLocalTime(value)
+    } else if (Object(value) === value) {
+      correctToLocal(value as Record<string, unknown>)
+    }
+  }
+}
+
+const correctToUTC = (param: Prisma.MiddlewareParams) => {
+  if (!param) return
+
+  for (const key of Object.keys(param)) {
+    const value = param[key]
+
+    if (value instanceof Date) {
+      param[key] = toUTCTime(value)
+    } else if (Object(value) === value) {
+      correctToUTC(value)
+    }
+  }
+}
+
+export const beforeMiddleware = (params: Prisma.MiddlewareParams) => {
+  correctToUTC(params)
+  return params
+}
+
+export const afterMiddleware = <T>(value: T) => {
+  if (value instanceof Date) {
+    return toLocalTime(value)
+  }
+
+  if (Object(value) !== value) {
+    return value
+  }
+
+  correctToLocal(value as Record<string, unknown>)
+
+  return value
+}
