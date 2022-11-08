b132c6d2b3ee4b76d527c46167fc84eed138b545
Tobias Beese <tobias.beese@gmx.de>
2022-11-08 13:58:46 +0100

    Adapt `DefectsPerPosition` component

---
+++ web/src/components/DefectsPerPosition/DefectsPerPosition.stories.tsx
@@ -0,0 +1,54 @@
+import DefectsPerPosition, {
+  DefectsPerPositionProps,
+} from './DefectsPerPosition'
+
+const args: DefectsPerPositionProps = {
+  partsPerSide: 6,
+  sumPerSide: new Map(),
+  sumPerPosition: new Map(),
+}
+
+export default { title: 'Components/DefectsPerPosition' }
+
+export const empty = () => {
+  return (
+    <div className="w-96">
+      <DefectsPerPosition {...args} />
+    </div>
+  )
+}
+
+export const sample = () => {
+  return (
+    <div className="w-96">
+      <DefectsPerPosition
+        {...args}
+        {...{
+          sumPerSide: new Map([
+            [1, 4],
+            [2, 4],
+          ]),
+          sumPerPosition: new Map([
+            [1, 2],
+            [5, 2],
+            [12, 4],
+          ]),
+        }}
+      />
+    </div>
+  )
+}
+
+export const longText = () => {
+  return (
+    <div className="w-96">
+      <DefectsPerPosition
+        {...args}
+        {...{
+          sumPerSide: new Map([[1, 4_000_000]]),
+          sumPerPosition: new Map([[6, 'This is a very long label']]),
+        }}
+      />
+    </div>
+  )
+}
--- web/src/components/DefectsPerPosition/DefectsPerPosition.tsx
+++ web/src/components/DefectsPerPosition/DefectsPerPosition.tsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react'
+
 import { max } from 'd3'
 
+import { CustomizationContext } from 'src/providers/context/CustomizationContext'
 import { getColorFor } from 'src/utils/getColorFor'
 
 import { Block } from './Block'
 import { Side } from './Side'
 
-export type Props = {
+export type DefectsPerPositionProps = {
   partsPerSide: number
   sumPerPosition: Map<number, string | number>
   sumPerSide: Map<number, number>
@@ -15,7 +18,7 @@ export type Props = {
   onSetSide?: (side: number) => void
 }
 
-const SkidPositionComponent = ({
+const DefectsPerPosition = ({
   partsPerSide,
   sumPerSide,
   sumPerPosition,
@@ -23,7 +26,9 @@ const SkidPositionComponent = ({
   activeSide,
   onSetPos,
   onSetSide,
-}: Props) => {
+}: DefectsPerPositionProps) => {
+  const { doubleSidedSkids } = useContext(CustomizationContext)
+
   const maxValue = max([...sumPerPosition.values()].map((v) => +v))
   const getColor = getColorFor(maxValue || 0)
 
@@ -31,57 +36,80 @@ const SkidPositionComponent = ({
   // const getPercent = getPercentFor(maxValue || 0)
 
   return (
-    <div className="mt-2 mb-5 grid min-w-[260px] grid-cols-2 gap-2 bg-white p-2 shadow-xl">
-      <Side
-        side={1}
-        count={sumPerSide.get(1)}
-        activeSide={activeSide}
-        onSetSide={onSetSide}
-      >
-        {[...Array(partsPerSide)].map((_empty, index) => {
-          const pos = partsPerSide - index
-          const count = sumPerPosition.get(pos)
+    <>
+      {doubleSidedSkids ? (
+        <div className="mt-2 mb-5 grid min-w-[260px] grid-cols-2 gap-2 bg-white p-2 shadow-xl">
+          <Side
+            side={1}
+            count={sumPerSide.get(1)}
+            activeSide={activeSide}
+            onSetSide={onSetSide}
+          >
+            {[...Array(partsPerSide)].map((_empty, index) => {
+              const pos = partsPerSide - index
+              const count = sumPerPosition.get(pos)
 
-          return (
-            <Block
-              position={pos}
-              count={count}
-              activePos={activePos}
-              getColor={getColor}
-              onSetPos={onSetPos}
-              key={pos}
-            />
-          )
-        })}
-      </Side>
+              return (
+                <Block
+                  position={pos}
+                  count={count}
+                  activePos={activePos}
+                  getColor={getColor}
+                  onSetPos={onSetPos}
+                  key={pos}
+                />
+              )
+            })}
+          </Side>
 
-      <Side
-        side={2}
-        count={sumPerSide.get(2)}
-        activeSide={activeSide}
-        onSetSide={onSetSide}
-      >
-        {[...Array(partsPerSide)].map((_empty, index) => {
-          const pos = partsPerSide * 2 - index
-          const count = sumPerPosition.get(pos)
+          <Side
+            side={2}
+            count={sumPerSide.get(2)}
+            activeSide={activeSide}
+            onSetSide={onSetSide}
+          >
+            {[...Array(partsPerSide)].map((_empty, index) => {
+              const pos = partsPerSide * 2 - index
+              const count = sumPerPosition.get(pos)
 
-          return (
-            <Block
-              position={pos}
-              count={count}
-              activePos={activePos}
-              getColor={getColor}
-              onSetPos={onSetPos}
-              key={pos}
-            />
-          )
-        })}
-      </Side>
-    </div>
+              return (
+                <Block
+                  position={pos}
+                  count={count}
+                  activePos={activePos}
+                  getColor={getColor}
+                  onSetPos={onSetPos}
+                  key={pos}
+                />
+              )
+            })}
+          </Side>
+        </div>
+      ) : (
+        <div className="mt-2 mb-5 min-w-[130px] bg-white p-2 shadow-xl">
+          <div className="flex flex-col gap-1">
+            {[...Array(partsPerSide)].map((_empty, index) => {
+              const pos = partsPerSide - index
+              const count = sumPerPosition.get(pos)
+              return (
+                <Block
+                  position={pos}
+                  count={count}
+                  activePos={activePos}
+                  getColor={getColor}
+                  onSetPos={onSetPos}
+                  key={pos}
+                />
+              )
+            })}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
 // const getPercentFor = (maxValue: number) => (value: number) =>
 //   `${(value / maxValue) * 100}%`
 
-export default SkidPositionComponent
+export default DefectsPerPosition
