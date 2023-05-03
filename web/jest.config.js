399bf5aab7dd577c2db345be29ebb812ee1962d3
Tobias Beese <tobias.beese@gmx.de>
2023-05-03 10:51:53 +0200

    Setup jest mocking for `react-i18next`

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,1 +1,8 @@
-module.exports = require('@redwoodjs/testing/config/jest/web')
+const config = {
+  rootDir: '../',
+  preset: '@redwoodjs/testing/config/jest/web',
+  // ☝️ load the built-in Redwood Jest configuration
+  setupFiles: ['./web/setupMock.js'],
+}
+
+module.exports = config
