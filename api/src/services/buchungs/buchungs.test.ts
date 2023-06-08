40bae2ba278bf84147ce94de3422f9fd58b2b6c3
Tobias Beese <tobias.beese@gmx.de>
2023-06-08 10:46:21 +0200

    Remove `buchungs` service
    Fetching **all** entries is never needed.

--- api/src/services/buchungs/buchungs.test.ts
+++ api/src/services/buchungs/buchungs.test.ts
@@ -1,13 +1,5 @@
-import { buchungs, getAllCodes } from './buchungs'
+import { getAllCodes } from './buchungs'
 import type { StandardScenario } from './buchungs.scenarios'
-
-describe('buchungs', () => {
-  scenario('returns all buchungs', async (scenario: StandardScenario) => {
-    const result = await buchungs()
-
-    expect(result.length).toEqual(Object.keys(scenario.buchung).length)
-  })
-})
 
 describe('getAllCodes', () => {
   scenario(
--- api/src/services/buchungs/buchungs.ts
+++ api/src/services/buchungs/buchungs.ts
@@ -1,9 +1,5 @@
 import { db } from 'src/lib/db'
 
-export const buchungs = () => {
-  return db.buchung.findMany()
-}
-
 export const getAllCodes = () => {
   return db.buchung.findMany({
     distinct: ['artikelcode', 'artikelbezeichnung'],
