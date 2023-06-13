d5fee4a8a7e77f3c35535ebb870543eef24df25c
Tobias Beese <tobias.beese@gmx.de>
2023-06-13 16:19:09 +0200

    Simplify `SummaryLabel`

--- web/src/components/Groupings/SummaryLabel.tsx
+++ web/src/components/Groupings/SummaryLabel.tsx
@@ -8,9 +8,7 @@ export type SummaryLabelProps = {
 const SummaryLabel = ({ count, label, euros, percent }: SummaryLabelProps) => {
   return (
     <div className="pb-1 pl-1 pr-2">
-      <div className="flex">
-        <span className="truncate text-base font-semibold">{label}</span>
-      </div>
+      <span className="truncate text-base font-semibold">{label}</span>
 
       <div className="flex items-baseline justify-between gap-2">
         <span className="tracking-tighter">{percent ? percent : count}</span>
