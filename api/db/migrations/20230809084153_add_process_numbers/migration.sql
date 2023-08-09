cbacf8d65a8d0fe3c069d8c771b947a39946b3a0
Tobias Beese <tobias.beese@gmx.de>
2023-08-09 11:05:43 +0200

    Migrate db table `lack5_export`

---
+++ api/db/migrations/20230809084153_add_process_numbers/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `lack5_export` ADD COLUMN `NachArbeitsplatz` VARCHAR(10) NULL,
+    ADD COLUMN `NachProzess` VARCHAR(10) NULL,
+    ADD COLUMN `VonArbeitsplatz` VARCHAR(10) NULL,
+    ADD COLUMN `VonProzess` VARCHAR(10) NULL;
